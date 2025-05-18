cde736624dc5853f11a853dc07ff754086313c16
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-18 21:48:18 +0800

    #

--- CSDS101_Final_Output.py
+++ CSDS101_Final_Output.py
@@ -5,7 +5,71 @@ import mysql.connector # ito po ay para magamit ang MySQL sa program
 from mysql.connector import errorcode # ito po ay para makapag-handle ng errors sa SQL database
 import os # ito po ay para magkaroon ng access sa operating system functionalities, tulad po ng pag clear ng screen ng console
 
+class Blood_Donation_DB: # ito po yung class na kailangan para magfunction yung buong program
+    def __init__(self, user: str, password: str, host: str, database: str): # ito po ay para ma-initialize yung class
+        self.user = user # ito po ay para ma-initialize at ma-encapsulate yung user ng MySQL database
+        self.password = password # ito po ay para ma-initialize at ma-encapsulate yung password ng MySQL database
+        self.host = host # ito po ay para ma-initialize at ma-encapsulate yung host ng MySQL database
+        self.database = database # ito po ay para ma-initialize at ma-encapsulate yung schema ng MySQL database
 
+    @property
+    def get_user(self):
+        return self.user
+    
+    @get_user.setter
+    def set_user(self, user):
+        self.user = user
+
+    @property
+    def get_password(self):
+        return self.password
+
+    @get_password.setter
+    def set_password(self, password):
+        self.password = password
+    
+    @property
+    def get_host(self):
+        return self.host
+    
+    @get_host.setter
+    def set_host(self, host):
+        self.host = host
+
+    @property
+    def get_database(self):
+        return self.database
+    
+    @get_database.setter
+    def set_database(self, database):
+        self.database = database
+
+    def connect(self): # ito po ay para magconnect sa MySQL 
+        try: # ito po ay para sa tingnan kung succesful ang connection sa database
+            con = mysql.connector.connect(user=self.user, # ito po ay para magamit ang nakuhang "user" sa class initialization
+                                        password=self.password,  # ito po ay para magamit ang nakuhang "password" sa class initialization
+                                        host=self.host, # ito po ay para magamit ang nakuhang "host" sa class initialization
+                                        database=self.database) # ito po ay para magamit ang nakuhang "database" sa class initialization
+            print('Connection successful') # ito po ay para malaman na successful ang pag-connect sa database
+            return con # ito po ay para ibalik ang naging successful connection sa connect function
+        except mysql.connector.Error as err: # ito po ay saluhin ng ang error sa pag-connect sa database
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR: # ito po ay para i-handle ang maling input ng credentials sa database
+                print("Something is wrong with your user name or password") # ito po para sabihin na may mali sa user or password 
+            elif err.errno == errorcode.ER_BAD_DB_ERROR: # ito po ay para i-handle ang pag connect sa non-existent database
+                print("Database does not exist") # ito po para sabihin na wala sa SQL yung database
+            else: # ito po ay para masalo ang iba pang errors
+                print(err) # ito po ay para ibigay ang specific error na meron sa SQL database
+
+
+def DB_credentials(): # ito po ay para i-require ang user na ibigay the credentials ng SQL database 
+    user = input('Enter user: ') # ito po ay para kunin ang user
+    password = input('Enter password: ') # ito po ay para kunin ang password
+    host = input('Enter host: ') # ito po ay para kunin ang host
+    db_select = input('Enter Database: ') # ito po ay para kunin ang schema 
+
+    # SQL_Blood_Donation_DB = Blood_Donation_DB('root', 'CS2025EU', 'localhost', 'Pre_Enrollees')
+    global Blood_Donation_DB # ito po ay para ma-access ang SQL_Blood_Donation_DB sa menu
+    SQL_Blood_Donation_DB = Blood_Donation_DB(user, password, host, db_select) # ito po ay para magamit yung class Blood_Donation_DB 
 
 def main(): # ito po yung main menu
     while True: # ito po ay para ma-display lagi yung main menu 
