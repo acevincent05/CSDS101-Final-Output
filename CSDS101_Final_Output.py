cfd837a9a69ca3d7c8986d3e28da6c742bba298b
Ace Gamitin <gamitinacevincent@gmail.com>
2025-05-18 21:08:48 +0800

    # import modules

--- CSDS101_Final_Output.py
+++ CSDS101_Final_Output.py
@@ -1,2 +1,7 @@
 # Gamitin, Ace Vincent A. 
 # CSDS101 Final Output 
+
+import mysql.connector # ito po ay para magamit ang MySQL sa program
+from mysql.connector import errorcode # ito po ay para makapag-handle ng errors sa SQL database
+import os # ito po ay para magkaroon ng access sa operating system functionalities, tulad po ng pag clear ng screen ng console
+
